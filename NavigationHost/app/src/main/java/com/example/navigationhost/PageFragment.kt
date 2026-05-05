19da3f58694f1b0aaa026ae43cad5757ec273222
Fiezz65 <hfzzprr@gmail.com>
2026-05-05 20:18:33 +0800

    Navigation Host

---
+++ NavigationHost/app/src/main/java/com/example/navigationhost/PageFragment.kt
@@ -0,0 +1,32 @@
+package com.example.navigationhost
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import kotlin.getValue
+
+class PageFragment : Fragment() {
+
+    val args: PageFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(args.layoutResId, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btn = view.findViewById<View>(args.btnId)
+        btn?.setOnClickListener {
+            findNavController().navigate(args.btnAction)
+        }
+    }
+}
